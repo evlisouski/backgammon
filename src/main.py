4ed1b8c86b7647c25848da798435ee17f703fb9e
Eduard <evlisouski@gmail.com>
2024-05-10 01:07:45 +0300

    feat: added pydantic schemas and routers for score endpoints

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from src.score.router import router as router_score
 
 app = FastAPI()
+
+app.include_router(router_score)
 
 
 @app.get("/")
---
+++ src/score/schemas.py
@@ -0,0 +1,33 @@
+import uuid
+from enum import Enum
+from typing import Dict, List, Optional
+from uuid import UUID
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
+
+
+class SBoardPoint(BaseModel):
+    number: Annotated[int, Field(ge=0, le=23)]
+    checkers_count: Annotated[int, Field(ge=0, le=15)]
+    occupied_by: Optional[UUID] = None
+
+
+class SBoard(BaseModel):
+    bar_counts: Dict[UUID, Annotated[int, Field(ge=0, le=23)]] = {uuid.uuid4(): 1, }
+    points: List[SBoardPoint]
+
+
+class SGameResultInput(BaseModel):
+    board: SBoard
+    start_position: Dict[UUID, Annotated[int, Field(ge=0, le=23)]] = {uuid.uuid4(): 1, }
+
+
+class GameWinType(str, Enum):
+    oin = 'oin'
+    mars = 'mars'
+    koks = 'koks'
+
+
+class SGameResultOutput(BaseModel):
+    points: Annotated[int, Field(ge=0)]
+    win_type: GameWinType
